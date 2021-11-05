4b6cf25a29b7b558cf667abe91230b6bba7a9787
segaldor <70015875+segaldor@users.noreply.github.com>
2021-11-05 17:30:45 +0200

    Add files via upload

---
+++ 02_SolutionStarter-dor - Copy/FacebookWinFormsApp/FormLogin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    public partial class FormLogin : Form
+    {
+		public User m_LoggedInUser;
+		public bool login_success = false;
+        public FormLogin()
+        {
+            InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 100;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+			loginAndInit();
+        }
+		private void loginAndInit()
+		{
+			FacebookWrapper.LoginResult loginResult = FacebookService.Login("615568443218854",
+			   "public_profile",
+				   "email",
+				   "publish_to_groups",
+				   "user_birthday",
+				   //"user_age_range",
+				   "user_gender",
+				   //"user_link",
+				   //"user_videos",
+				   //"publish_to_groups",
+				   //"groups_access_member_info",
+				   //"user_friends",
+				   //"user_events",
+				   "user_likes",
+				   //"user_location",
+				   "user_photos",
+				   "user_posts"
+				   //"user_hometown"
+				   );
+			if (!string.IsNullOrEmpty(loginResult.AccessToken))
+			{
+				m_LoggedInUser = loginResult.LoggedInUser;
+				login_success = true;
+				this.Dispose();
+			}
+			else 
+			{
+				MessageBox.Show(loginResult.ErrorMessage, "Login faild");
+			}
+
+				
+		}
+    }
+}
