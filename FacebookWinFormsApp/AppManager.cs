3291c0af111e17517e84c69f8cfb3aba16187285
segaldor <70015875+segaldor@users.noreply.github.com>
2021-11-05 17:41:59 +0200

    Add files via upload

---
+++ FacebookWinFormsApp/AppManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    public class AppManager
+    {
+        User m_LoggedInUser;
+        LoginResult m_LoginResult;
+        FormLogin m_FormLogin;
+        FormMain m_FormMain;
+
+        
+        
+
+        public AppManager() 
+        {   
+            m_FormLogin = new FormLogin();
+            m_FormLogin.ShowDialog();
+            if (m_FormLogin.login_success)
+            {
+                m_LoggedInUser = m_FormLogin.m_LoggedInUser;
+                m_FormMain = new FormMain(m_LoggedInUser);
+                m_FormMain.ShowDialog();
+            }
+        }
+                
+    }
+   
+}
---
+++ FacebookWinFormsApp/FormLogin.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace BasicFacebookFeatures
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.AccessibleName = "buttonLogin";
+            this.buttonLogin.Location = new System.Drawing.Point(210, 326);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(352, 56);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(713, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome to Dor and Noa Facebook desktop app";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(53, 254);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(231, 20);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "please login in the button below";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label loginLabel;
+    }
+}
